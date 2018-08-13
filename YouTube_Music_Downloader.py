2282c0c035bd07e447eb52dfcdfb22a606a586c0
mfzhang412 <mfzhang412@users.noreply.github.com>
2018-08-13 00:01:49 -0500

    Applied PEP 8

--- YouTube_Music_Downloader.py
+++ YouTube_Music_Downloader.py
@@ -4,8 +4,11 @@ import tkinter
 from tkinter import filedialog
 import requests
 
+
 def choose():
-    """Asks user if they want to download a YouTube video's audio file or an entire YouTube playlist's audio files"""
+    """Asks user if they want to download a YouTube video's audio file or an entire YouTube playlist's audio files.
+    :return: the user's choice (1 for playlist, 2 for individual, -1 for an invalid option).
+    """
     msg = """
     Which would you like to do (enter 1 or 2):
     (1) Playlist: download all the audio files from a YouTube playlist
@@ -16,17 +19,25 @@ def choose():
     if ((choice != 1) and (choice != 2)): return -1    #return -1 if invalid option
     return choice
 
+
 def askSaveLocation():
-    """Brings up dialogue to select folder for saving files"""
+    """Brings up dialogue to select folder for saving files.
+    :return: the directory path for the save location.
+    """
     root = tkinter.Tk()
+    root.attributes("-topmost", True)
     root.withdraw()     #hide root window
     print("Please select a save location.")
     path = filedialog.askdirectory(parent=root, title='Please select a save location')
     root.destroy()      #destroy root window
     return path
 
+
 def getPlaylistURLs(playlist_url):  #courtesy of https://gist.github.com/fffaraz/f3dcf48ae93b6c04adb9d74b1de711e5
-    """Returns all the video urls and video names in the YouTube playlist"""
+    """Returns all the video urls and video names in the YouTube playlist.
+    :param playlist_url: the YouTube playlist url.
+    :return: a list of the video names and video urls from the playlist.
+    """
     video_names = []
     video_urls = []
     source = requests.get(playlist_url).text
@@ -39,8 +50,13 @@ def getPlaylistURLs(playlist_url):  #courtesy of https://gist.github.com/fffaraz
             video_names.append(link.string.strip())     #append video names
     return [video_names,video_urls]
 
-def downloadVideoAudio(youtube_url,save_location):
-    """Download the highest quality audio stream from the youtube video"""
+
+def downloadVideoAudio(youtube_url, save_location):
+    """Download the highest quality audio stream from the youtube video.
+    :param youtube_url: the YouTube URL.
+    :param save_location: the directory path for the save location.
+    :return: true if song successfully downloaded, false otherwise.
+    """
     yt = YouTube(youtube_url)
     try:
         yt.streams.filter(only_audio=True).first().download(save_location)   #downloads the highest quality audio stream and saves to designated folder
@@ -50,8 +66,13 @@ def downloadVideoAudio(youtube_url,save_location):
         print("---ERROR---" + video_names[i] + "\t\t" + video_urls[i])
         return False
 
-def downloadPlaylistAudio(playlist_url,save_location):    #courtesy of https://github.com/nficano/pytube
-    """Download the audio files of all the videos in a playlist"""
+
+def downloadPlaylistAudio(playlist_url, save_location):    #courtesy of https://github.com/nficano/pytube
+    """Download the audio files of all the videos in a playlist.
+    :param playlist_url: the YouTube playlist URL.
+    :param save_location: the directory path for the save location.
+    :return: true if playlist successfully downloaded, false otherwise.
+    """
     l = getPlaylistURLs(playlist_url)
     video_names = l[0]
     video_urls = l[1]
@@ -65,11 +86,10 @@ def downloadPlaylistAudio(playlist_url,save_location):    #courtesy of https://g
     return True
 
 
-
-
-
 def main():
-    """Main method"""
+    """Entry point for program.
+    :return: None; breaks out of method if error arises.
+    """
     #playlist_url = input("Enter YouTube playlist url: ")
     choice = choose()
     if choice == 1:
@@ -92,6 +112,7 @@ def main():
             print("\n\nSong finished downloading!")
     except:
         print("Error: possibly invalid URL")
+
 
 if __name__ == "__main__":
     main()
